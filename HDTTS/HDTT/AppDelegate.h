f01bb0b4d3039071e4609c272934f603e54dcd6e
gsbios <15217636961@163.com>
2019-09-11 11:45:06 +0800

    9.11

---
+++ HDTTS/HDTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDTT
+//
+//  Created by gsb on 2019/9/11.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDTTS/HDTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDTT
+//
+//  Created by gsb on 2019/9/11.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
